ab42b715ad66a2d14ef1c705eb06e6e88e45b28b
Soumya Rayast <113257526+soumya-rayast@users.noreply.github.com>
2024-02-29 15:52:12 +0530

    header part completed

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let searchform = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () =>{
     searchform.classList.toggle('active');
+    navbar.classList.remove('active')
+    cart.classList.remove('active')
+    loginform.classList.remove('active');
 }
 
 // for cart
@@ -11,4 +14,38 @@ let cart = document.querySelector('.shopping-cart');
 
 document.querySelector("#cart-btn").onclick = () =>{
     cart.classList.toggle('active')
+    navbar.classList.remove('active')
+    searchform.classList.remove('active');
+    loginform.classList.remove('active');
+}
+
+// for login form 
+
+let loginform = document.querySelector('.login-form');
+
+document.querySelector("#login-btn").onclick = () =>{
+    loginform.classList.toggle('active');
+    navbar.classList.remove('active')
+    searchform.classList.remove('active');
+    cart.classList.remove('active')
+}
+
+// in media querries 
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector("#menu-btn").onclick = () =>{
+    navbar.classList.toggle('active');
+    searchform.classList.remove('active');
+    cart.classList.remove('active')
+    loginform.classList.remove('active');
+}
+
+// for rmove active classes
+
+window.onscroll = () =>{
+    navbar.classList.remove('active')
+    searchform.classList.remove('active');
+    cart.classList.remove('active')
+    loginform.classList.remove('active');
 }
