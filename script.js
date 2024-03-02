62a621cc057eefdb3e450800c468d650d9809fae
Soumya Rayast <113257526+soumya-rayast@users.noreply.github.com>
2024-03-03 00:20:38 +0530

    Home page completed

--- script.js
+++ script.js
@@ -49,3 +49,22 @@ window.onscroll = () =>{
     cart.classList.remove('active')
     loginform.classList.remove('active');
 }
+
+
+// main section slides
+
+
+let slides = document.querySelectorAll('.home .slides-container .slide');
+let index = 0;
+
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active')
+}
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active')
+}
